3de76a40a0f069d7d1139295bd975bd80771283b
Celmira Quaresma <CelmiraQuaresmaa@gmail.com>
2024-05-14 17:49:06 +0100

    For-Loops For Waves

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -19,5 +20,10 @@ public class Enemy : MonoBehaviour
     {
         Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         enemyRb.AddForce( lookDirection * speed); 
+
+        if(transform.position.y < -10)
+        {
+            Destroy(gameObject);
+        }
     }
 }
